a184e7eb3d6ed641cac9286fd457b9921eee72f9
IYP-Programer-Yeah <hgsilvermann@yahoo.com>
2020-07-27 20:09:41 +0430

    Replace SizableAlignableObject with std::aligned_storage
    Fixes #7

--- Test/Source/cache-aligned-and-padded-object-test.cpp
+++ Test/Source/cache-aligned-and-padded-object-test.cpp
@@ -1,27 +1,28 @@
+#define _ENABLE_EXTENDED_ALIGNED_STORAGE
+
 #include <Iyp/WaitFreeRingBufferUtilities/details/cache-aligned-and-padded-object.inl>
 #include <gtest/gtest.h>
+
+#include <type_traits>
 
 namespace Iyp
 {
 namespace CacheAlignedAndPaddedObjectTest
 {
-template <std::size_t Alignment, std::size_t Size>
-struct alignas(Alignment) SizableAlignableObject
-{
-    std::uint8_t padding[Size];
-};
-
 TEST(CacheAlignedAndPaddedObjectTest, UnalignedWithUndevisableSizeObject)
 {
-    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_ALIGNMENT / 2;
-    static constexpr std::size_t Size = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_LINE_SIZE / 2;
+    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE / 2;
+    static constexpr std::size_t Size = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE / 2;
 
-    using UnalignedWithUndevisableSizeObjectType = SizableAlignableObject<Alignment, Size>;
+    struct UnalignedWithUndevisableSizeObjectType
+    {
+        typename std::aligned_storage<Size, Alignment>::type data;
+    };
     EXPECT_EQ(Alignment, alignof(UnalignedWithUndevisableSizeObjectType));
     EXPECT_EQ(Size, sizeof(UnalignedWithUndevisableSizeObjectType));
 
-    static constexpr std::size_t ExpectedAlignment = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_ALIGNMENT;
-    static constexpr std::size_t ExpectedSize = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_LINE_SIZE;
+    static constexpr std::size_t ExpectedAlignment = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE;
+    static constexpr std::size_t ExpectedSize = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE;
     using CacheAlignedAndPaddedObjectType = Iyp::WaitFreeRingBufferUtilities::Details::CacheAlignedAndPaddedObject<UnalignedWithUndevisableSizeObjectType>;
     EXPECT_EQ(ExpectedAlignment, alignof(CacheAlignedAndPaddedObjectType));
     EXPECT_EQ(ExpectedSize, sizeof(CacheAlignedAndPaddedObjectType));
@@ -29,14 +30,17 @@ TEST(CacheAlignedAndPaddedObjectTest, UnalignedWithUndevisableSizeObject)
 
 TEST(CacheAlignedAndPaddedObjectTest, UnalignedWithDevisableSizeObject)
 {
-    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_ALIGNMENT / 2;
-    static constexpr std::size_t Size = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_LINE_SIZE * 3;
+    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE / 2;
+    static constexpr std::size_t Size = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE * 3;
 
-    using UnalignedWithUndevisableSizeObjectType = SizableAlignableObject<Alignment, Size>;
+    struct UnalignedWithUndevisableSizeObjectType
+    {
+        typename std::aligned_storage<Size, Alignment>::type data;
+    };
     EXPECT_EQ(Alignment, alignof(UnalignedWithUndevisableSizeObjectType));
     EXPECT_EQ(Size, sizeof(UnalignedWithUndevisableSizeObjectType));
 
-    static constexpr std::size_t ExpectedAlignment = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_ALIGNMENT;
+    static constexpr std::size_t ExpectedAlignment = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE;
     static constexpr std::size_t ExpectedSize = Size;
     using CacheAlignedAndPaddedObjectType = Iyp::WaitFreeRingBufferUtilities::Details::CacheAlignedAndPaddedObject<UnalignedWithUndevisableSizeObjectType>;
     EXPECT_EQ(ExpectedAlignment, alignof(CacheAlignedAndPaddedObjectType));
@@ -45,15 +49,18 @@ TEST(CacheAlignedAndPaddedObjectTest, UnalignedWithDevisableSizeObject)
 
 TEST(CacheAlignedAndPaddedObjectTest, DISABLED_AlignedWithUndevisableSizeObject)
 {
-    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_ALIGNMENT;
-    static constexpr std::size_t Size = (Iyp::WaitFreeRingBufferUtilities::Details::CACHE_LINE_SIZE / 2) * 3;
+    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE;
+    static constexpr std::size_t Size = (Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE / 2) * 3;
 
-    using UnalignedWithUndevisableSizeObjectType = SizableAlignableObject<Alignment, Size>;
+    struct UnalignedWithUndevisableSizeObjectType
+    {
+        typename std::aligned_storage<Size, Alignment>::type data;
+    };
     EXPECT_EQ(Alignment, alignof(UnalignedWithUndevisableSizeObjectType));
     EXPECT_EQ(Size, sizeof(UnalignedWithUndevisableSizeObjectType));
 
     static constexpr std::size_t ExpectedAlignment = Alignment;
-    static constexpr std::size_t ExpectedSize = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_LINE_SIZE * 2;
+    static constexpr std::size_t ExpectedSize = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE * 2;
     using CacheAlignedAndPaddedObjectType = Iyp::WaitFreeRingBufferUtilities::Details::CacheAlignedAndPaddedObject<UnalignedWithUndevisableSizeObjectType>;
     EXPECT_EQ(ExpectedAlignment, alignof(CacheAlignedAndPaddedObjectType));
     EXPECT_EQ(ExpectedSize, sizeof(CacheAlignedAndPaddedObjectType));
@@ -61,10 +68,13 @@ TEST(CacheAlignedAndPaddedObjectTest, DISABLED_AlignedWithUndevisableSizeObject)
 
 TEST(CacheAlignedAndPaddedObjectTest, AlignedWithDevisableSizeObject)
 {
-    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_ALIGNMENT;
-    static constexpr std::size_t Size = Iyp::WaitFreeRingBufferUtilities::Details::CACHE_LINE_SIZE * 3;
+    static constexpr std::size_t Alignment = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE;
+    static constexpr std::size_t Size = Iyp::WaitFreeRingBufferUtilities::Details::DESTRUCTIVE_INTERFERENCE_SIZE * 3;
 
-    using UnalignedWithUndevisableSizeObjectType = SizableAlignableObject<Alignment, Size>;
+    struct UnalignedWithUndevisableSizeObjectType
+    {
+        typename std::aligned_storage<Size, Alignment>::type data;
+    };
     EXPECT_EQ(Alignment, alignof(UnalignedWithUndevisableSizeObjectType));
     EXPECT_EQ(Size, sizeof(UnalignedWithUndevisableSizeObjectType));
 
