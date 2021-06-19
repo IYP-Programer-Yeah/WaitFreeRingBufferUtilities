69a03632af3c20444d9ec17f73c698526fe8b895
Hosein Ghahremanzadeh <hgsilverman@gmail.com>
2021-06-19 17:32:38 +0430

    Improve benchmark to actually measure throughput.

--- Example/RingBenchmark/Source/main.cpp
+++ Example/RingBenchmark/Source/main.cpp
@@ -5,6 +5,11 @@
 #include <cstddef>
 #include <cstdlib>
 #include <ctime>
+#include <list>
+#include <atomic>
+#include <thread>
+#include <cstdint>
+#include <utility>
 
 static constexpr std::size_t RingSize = 1024;
 
@@ -28,92 +33,117 @@ using ScspRingBufferType = Iyp::WaitFreeRingBufferUtilities::RingBuffer<Iyp::Wai
                                                                         std::size_t,
                                                                         RingSize>;
 
-template <typename RingType>
-struct RingFixture : benchmark::Fixture
+enum class ThreadType
 {
-    RingType ring;
-    std::size_t value_to_push;
+    PRODUCER,
+    CONSUMER,
+};
 
-    void SetUp(const ::benchmark::State &state)
+template <typename RingType>
+class Thread
+{
+    enum : std::uint8_t
     {
-        std::srand(std::time(0));
-        value_to_push = std::rand();
-        if (state.thread_index == 0)
+        START_ITERATION,
+        ENDED_ITERATION,
+    };
+
+    RingType &ring;
+    std::atomic_uint8_t signal;
+    std::atomic_bool should_stop;
+    std::size_t number_of_processed_elements_per_iteration;
+    std::thread thread;
+
+    void producer_thread()
+    {
+        while (!should_stop)
+            if (signal == START_ITERATION)
+            {
+                for (std::size_t i = 0; i < number_of_processed_elements_per_iteration; i++)
+                    while (!ring.push(i))
+                    {
+                    }
+
+                signal = ENDED_ITERATION;
+            }
+    }
+
+    void consumer_thread()
+    {
+        while (!should_stop)
+            if (signal == START_ITERATION)
+            {
+                for (std::size_t i = 0; i < number_of_processed_elements_per_iteration; i++)
+                    while (!ring.pop())
+                    {
+                    }
+
+                signal = ENDED_ITERATION;
+            }
+    }
+
+public:
+    Thread(RingType &i_ring,
+           const ThreadType thread_type,
+           const std::size_t i_number_of_processed_elements_per_iteration)
+        : ring(i_ring),
+          signal(ENDED_ITERATION),
+          should_stop(false),
+          number_of_processed_elements_per_iteration(i_number_of_processed_elements_per_iteration),
+          thread([this, thread_type]() { thread_type == ThreadType::PRODUCER ? producer_thread() : consumer_thread(); })
+    {
+    }
+
+    void run_an_iteration()
+    {
+        signal = START_ITERATION;
+    }
+
+    void wait_for_iteration_to_end()
+    {
+        while (signal != ENDED_ITERATION)
         {
-            for (std::size_t i = 0; i < RingSize / 2; i++)
-                ring.push(i);
         }
     }
 
-    void TearDown(const ::benchmark::State &state)
+    ~Thread()
     {
-        if (state.thread_index == 0)
+        should_stop = true;
+        while (!thread.joinable())
         {
-            while (ring.pop())
-                ;
         }
+        thread.join();
     }
 };
 
-BENCHMARK_TEMPLATE_DEFINE_F(RingFixture, scsp_multithreaded_benchmark, ScspRingBufferType)
-(benchmark::State &state)
+template <typename RingType>
+void throughput_benchmark(benchmark::State &state)
 {
-    if (state.thread_index == 0)
-        for (auto _ : state)
-            ring.push(value_to_push);
-    else
-        for (auto _ : state)
-            ring.pop();
-}
-BENCHMARK_REGISTER_F(RingFixture, scsp_multithreaded_benchmark)->Threads(2);
+    constexpr std::size_t NumberOfProcessedElementsPerIteration = RingSize * 8;
+    RingType ring;
+    for (std::size_t i = 0; i < RingSize / 2; i++)
+        ring.push(i);
 
-BENCHMARK_TEMPLATE_DEFINE_F(RingFixture, mcsp_multithreaded_benchmark, McspRingBufferType)
-(benchmark::State &state)
-{
-    if (state.thread_index == 0)
-        for (auto _ : state)
-            ring.push(value_to_push);
-    else
-        for (auto _ : state)
-            ring.pop();
-}
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(8);
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(7);
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(6);
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(5);
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(4);
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(3);
-BENCHMARK_REGISTER_F(RingFixture, mcsp_multithreaded_benchmark)->Threads(2);
+    std::list<Thread<RingType>> threads;
 
-BENCHMARK_TEMPLATE_DEFINE_F(RingFixture, scmp_multithreaded_benchmark, ScmpRingBufferType)
-(benchmark::State &state)
-{
-    if (state.thread_index == 0)
-        for (auto _ : state)
-            ring.pop();
-    else
-        for (auto _ : state)
-            ring.push(value_to_push);
-}
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(8);
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(7);
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(6);
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(5);
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(4);
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(3);
-BENCHMARK_REGISTER_F(RingFixture, scmp_multithreaded_benchmark)->Threads(2);
+    for (std::size_t i = 0; i < state.range(0); i++)
+        threads.emplace_back(ring, ThreadType::PRODUCER, NumberOfProcessedElementsPerIteration * state.range(1));
 
-BENCHMARK_TEMPLATE_DEFINE_F(RingFixture, mcmp_multithreaded_benchmark, McmpRingBufferType)
-(benchmark::State &state)
-{
-    if (state.thread_index < (state.threads / 2))
-        for (auto _ : state)
-            ring.push(value_to_push);
-    else
-        for (auto _ : state)
-            ring.pop();
+    for (std::size_t i = 0; i < state.range(1); i++)
+        threads.emplace_back(ring, ThreadType::CONSUMER, NumberOfProcessedElementsPerIteration * state.range(0));
+
+    for (auto _ : state)
+    {
+        for (auto &thread : threads)
+            thread.run_an_iteration();
+        for (auto &thread : threads)
+            thread.wait_for_iteration_to_end();
+    }
+
+    state.SetComplexityN((state.range(0) + state.range(1)) * NumberOfProcessedElementsPerIteration * state.range(0) * state.range(1));
 }
-BENCHMARK_REGISTER_F(RingFixture, mcmp_multithreaded_benchmark)->Threads(8);
-BENCHMARK_REGISTER_F(RingFixture, mcmp_multithreaded_benchmark)->Threads(6);
-BENCHMARK_REGISTER_F(RingFixture, mcmp_multithreaded_benchmark)->Threads(4);
-BENCHMARK_REGISTER_F(RingFixture, mcmp_multithreaded_benchmark)->Threads(2);
+
+BENCHMARK_TEMPLATE(throughput_benchmark, ScmpRingBufferType)->ArgsProduct({{1, 2, 3, 4, 5, 6, 7}, {1}})->ArgNames({"Producer Count", "Consumer Count"})->Complexity();
+BENCHMARK_TEMPLATE(throughput_benchmark, McspRingBufferType)->ArgsProduct({{1}, {1, 2, 3, 4, 5, 6, 7}})->ArgNames({"Producer Count", "Consumer Count"})->Complexity();
+BENCHMARK_TEMPLATE(throughput_benchmark, McmpRingBufferType)->ArgsProduct({{1, 2, 3, 4}, {1, 2, 3, 4}})->ArgNames({"Producer Count", "Consumer Count"})->Complexity();
+BENCHMARK_TEMPLATE(throughput_benchmark, ScspRingBufferType)->ArgsProduct({{1}, {1}})->ArgNames({"Producer Count", "Consumer Count"});
